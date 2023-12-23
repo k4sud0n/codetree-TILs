ab34a85ded5f490a7d8bd328cc7308f5c712acb2
Sihyun Kim <k4sud0n@gmail.com>
2023-12-24 00:15:35 +0900

    Title: 데이터 교환, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231224/데이터 교환/exchange-data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    b = a;
+    c = b;
+    a = c;
+
+    cout << a << endl << b << endl << c;
+    return 0;
+}
