70f633b0081a04a3a79394c089e4011dd9de4d0c
Sihyun Kim <k4sud0n@gmail.com>
2023-12-24 00:14:39 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231224/정수 복사/copy-integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 3;
+    int b = 4;
+    a = b;
+
+    cout << a << " " << b << endl;
+    cout << a * b << endl;
+    return 0;
+}
