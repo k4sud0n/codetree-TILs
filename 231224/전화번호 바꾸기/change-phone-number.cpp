e80693157bd3e18626f35fb955af595608b49c68
Sihyun Kim <k4sud0n@gmail.com>
2023-12-24 11:22:58 +0900

    Title: 전화번호 바꾸기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231224/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> res;
+    string phoneNumber, buffer;
+    cin >> phoneNumber;
+
+    istringstream ss;
+    ss.str(phoneNumber);
+
+    while(getline(ss, buffer, '-')) {
+        res.push_back(buffer);
+    }
+
+    for (auto i:v) cout << res[0] << endl;
+    for (auto i:v) cout << res[2] << endl;
+    for (auto i:v) cout << res[1] << endl;
+
+    return 0;
+}
