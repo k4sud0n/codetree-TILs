58759ca42c31c360abc332935d03ee72a17c776c
Sihyun Kim <k4sud0n@gmail.com>
2023-12-25 10:34:15 +0900

    Title: 삼항연산자 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231225/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a == 1) {
+        cout << "t" << endl;
+    } else {
+        cout << "f" << endl;
+    }
+
+    return 0;
+}
