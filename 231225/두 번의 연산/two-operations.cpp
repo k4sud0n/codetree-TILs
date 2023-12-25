963da74c696c2936c3ea52163902040557337feb
Sihyun Kim <k4sud0n@gmail.com>
2023-12-25 10:44:21 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231225/두 번의 연산/two-operations.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a % 2) {
+        a += 3;
+    }
+
+    if (a / 3) {
+        a /= 3;
+    }
+
+    cout << a << endl;
+
+    return 0;
+}
