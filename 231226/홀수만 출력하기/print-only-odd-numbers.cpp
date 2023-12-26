f53132b65b835a00b673dba60628eaa2c62f2752
Sihyun Kim <k4sud0n@gmail.com>
2023-12-26 11:30:44 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231226/홀수만 출력하기/print-only-odd-numbers.cpp
+++ 231226/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
     int n, input;
-    vector<int> v, input_v;
+    vector<int> v;
     cin >> n;
 
     for (int i = 0; i < n; i++) {
@@ -15,8 +14,6 @@ int main() {
             v.push_back(input);
         }
     }
-
-    sort(input_v.begin(), input_v.end());
 
     for (int i = 0; i < v.size(); i++) {
         cout << v[i] << endl;
