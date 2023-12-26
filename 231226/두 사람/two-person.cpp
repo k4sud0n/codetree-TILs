040532f95f57de7c65f22b208473f7929de19681
Sihyun Kim <k4sud0n@gmail.com>
2023-12-26 10:39:02 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231226/두 사람/two-person.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age1, age2;
+    char sex1, sex2;
+    cin >> age1 >> sex1;
+    cin >> age2 >> sex2;
+    cout << ((age1 >= 19 && sex1 == 'M') || (age2 >= 19 && sex2 == 'M')) << endl;
+    return 0;
+}
