bc7acd36088d3aeb201669b05bb5dc9a7a9d619a
Sihyun Kim <k4sud0n@gmail.com>
2023-12-28 23:48:32 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n * 2; i++) {
+        if (i % 2 != 0) {
+            for (int j = 1; j <= i; j++) {
+                cout << "*";
+            }
+            cout << "\n";
+        }
+    }
+
+    return 0;
+}
