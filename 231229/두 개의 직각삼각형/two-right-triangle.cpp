3f6912068e248b1ac1f80a7bee3520fd1aaeff9b
Sihyun Kim <k4sud0n@gmail.com>
2023-12-29 00:25:21 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j <= n - i; j++) cout << "*";
+        for(int k = 2; k <= i; k++) cout << "  ";
+        for(int l = 0; l <= n - i; l++) cout << "*";
+        cout << "\n";
+    }
+
+    return 0;
+}
