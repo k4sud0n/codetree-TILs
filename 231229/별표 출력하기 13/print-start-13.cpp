bb47c0078b06aa4bd1f58c7e439f8d26565693ea
Sihyun Kim <k4sud0n@gmail.com>
2023-12-29 11:46:06 +0900

    Title: 별표 출력하기 13, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231229/별표 출력하기 13/print-start-13.cpp
+++ 231229/별표 출력하기 13/print-start-13.cpp
@@ -7,21 +7,10 @@ int main() {
     cin >> n;
     
     for(int i = 0; i < n; i++) {
-        cout << "* ";
-    }
-    
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < i; j++) cout << "* ";
+        for(int j = 0; j < n - i; j++) cout << "* ";
         cout << "\n";
-    }
-
-    for(int i = n - 1; i > 0; i--) {
-        for(int j = 0; j < i; j++) cout << "* ";
+        for(int j = 0; j < i + 1; j++) cout << "* ";
         cout << "\n";
-    }
-
-    for(int i = 0; i < n; i++) {
-        cout << "* ";
     }
 
     return 0;
