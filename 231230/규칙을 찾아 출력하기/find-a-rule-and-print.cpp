0e4c264e7860750b257d15a9f53bcd26e31eb56b
Sihyun Kim <k4sud0n@gmail.com>
2023-12-30 00:26:02 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231230/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) cout << "* ";
+        for(int j = 0; j < n - i; j++) {
+            if(i >= 1) {
+                if(j == n - i - 1) {
+                    cout << "*";
+                } else {
+                    cout << "  ";
+                }
+            } else {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
