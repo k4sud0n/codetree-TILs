f066d70051b1ca85f496b106d86866f7f4e257b2
Sihyun Kim <k4sud0n@gmail.com>
2024-01-01 13:10:57 +0900

    Title: 두 숫자의 차의 최솟값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
+++ 240101/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -10,6 +10,7 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
+        if(n == 2) temp = arr[1] - arr[0];
         if(i == 2) temp = min(arr[i] - arr[i - 1], arr[i - 1] - arr[i - 2]);
         if(i > 2) temp = min(arr[i] - arr[i - 1], temp);
     }
