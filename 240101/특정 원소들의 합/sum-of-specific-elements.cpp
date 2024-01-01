3ae933a24727528a8830095d00ae507cc6d201c7
Sihyun Kim <k4sud0n@gmail.com>
2024-01-01 13:15:46 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240101/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[4][4];
+    int total = 0;
+
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) cin >> arr[i][j];
+    }
+
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j <= i; j++) total += arr[i][j];
+    }
+
+    cout << total;
+
+    return 0;
+}
