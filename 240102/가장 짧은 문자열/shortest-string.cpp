ac52f249c4c75f4e0f20f9760dd2580bbd6c2d99
Sihyun Kim <k4sud0n@gmail.com>
2024-01-02 16:03:00 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240102/가장 짧은 문자열/shortest-string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int min, max = 0;
+    string str;
+
+    for(int i = 0; i < 3; i++) {
+        cin >> str;
+        if(max < str.length()) max = str.length();
+        if(min > max) min = str.length();
+    }
+
+    cout << max - min;
+
+    return 0;
+}
