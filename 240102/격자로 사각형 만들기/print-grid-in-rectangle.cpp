c264e98af6e0a75945d10941b2e6b8f2e63283c1
Sihyun Kim <k4sud0n@gmail.com>
2024-01-02 11:19:46 +0900

    Title: 격자로 사각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    cin >> n;
+    int arr[n][n];
+
+    for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) arr[i][j] = 1;
+
+    for(int i = 1; i < n; i++) for(int j = 1; j < n; j++) arr[i][j] = arr[i - 1][j] + arr[i][j - 1] + arr[i - 1][j - 1];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
