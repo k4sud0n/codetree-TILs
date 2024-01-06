b9351f58191d09bd0e94e9bbd850955ad8dfa92e
Sihyun Kim <k4sud0n@gmail.com>
2024-01-06 13:55:27 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    cout << n * m / gcd(n, m);
+
+    return 0;
+}
