6dfcd8123b3f22364039d03bbfe67c997ac1fcda
Sihyun Kim <k4sud0n@gmail.com>
2024-01-09 20:38:55 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240109/비오는 날/rainy-day.cpp
+++ 240109/비오는 날/rainy-day.cpp
@@ -7,6 +7,7 @@ int main() {
     int n;
     int tempYear = 9999;
     int tempMonth = 12;
+    int tempDate = 31;
     string temp;
     cin >> n;
 
@@ -15,16 +16,21 @@ int main() {
         cin >> date >> day >> weather;
         int year = date[0] + date[1] + date[2] + date[3];
         int month = date[5] + date[6];
+        int ddate = date[8] + date[9];
 
         if(weather == "Rain") {
             if(year < tempYear) {
                 tempYear = year;
                 tempMonth = month;
+                tempDate = ddate;
                 temp = date + " " + day + " " + weather;
             } else {
                 if(year == tempYear) {
                     if(month < tempMonth) {
                         tempMonth = month;
+                        temp = date + " " + day + " " + weather;
+                    } else if(ddate < tempDate) {
+                        tempDate = ddate;
                         temp = date + " " + day + " " + weather;
                     }
                 }
