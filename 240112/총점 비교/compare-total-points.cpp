e2729b2636a8ca8980baf1d450a37ddb20da1746
Sihyun Kim <k4sud0n@gmail.com>
2024-01-12 12:10:22 +0900

    Title: 총점 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240112/총점 비교/compare-total-points.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Information {
+public:
+    string name;
+    int korean, english, math;
+
+    Information(string name = "name", int korean = 0, int english = 0, int math = 0) {
+        this->name = name;
+        this->korean = korean;
+        this->english = english;
+        this->math = math;
+    }
+
+    bool operator < (Information &information) {
+        return this->korean + this->english + this->math < information.korean + information.english + information.math;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Information informations[n];
+
+    for(int i = 0; i < n; i++) {
+        string name;
+        int korean, english, math;
+        cin >> name >> korean >> english >> math;
+        informations[i].name = name;
+        informations[i].korean = korean;
+        informations[i].english = english;
+        informations[i].math = math;
+    }
+
+    sort(informations, informations + n);
+
+    for(int i = 0; i < n; i++) cout << informations[i].name << " " << informations[i].korean << " " << informations[i].english << " " << informations[i].math << endl;
+
+    return 0;
+}
