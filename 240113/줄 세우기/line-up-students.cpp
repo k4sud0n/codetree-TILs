b7d328fc8be74d66f8fa59c32d0c02c5103195fd
Sihyun Kim <k4sud0n@gmail.com>
2024-01-13 11:23:40 +0900

    Title: 줄 세우기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240113/줄 세우기/line-up-students.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Student {
+public:
+    int index, h, w;
+
+    Student(int index = 0, int h = 0, int w = 0) {
+        this->index = index;
+        this->h = h;
+        this->w = w;
+    }
+
+    bool operator < (Student &student) {
+        if(this->h < student.h) {
+            return student.h;
+        } else if(this->h == student.h) {
+            if(this->w == student.w) return this->index > student.index;
+            else return this->w < student.w;
+        } else {
+            return student.h;
+        }
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    Student students[n];
+
+    for(int i = 0; i < n; i++) {
+        int h, w;
+        cin >> h >> w;
+        students[i].index = i + 1;
+        students[i].h = h;
+        students[i].w = w;
+    }
+
+    sort(students, students + n);
+
+    for(int i = 0; i < n; i++) cout << students[i].h << " " << students[i].w << " " << students[i].index << endl;
+
+    return 0;
+}
