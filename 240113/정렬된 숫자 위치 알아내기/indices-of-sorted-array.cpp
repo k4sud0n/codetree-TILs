56f27d7a00f70c254d9e07e2b73cbe1ce56177af
Sihyun Kim <k4sud0n@gmail.com>
2024-01-13 13:25:46 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240113/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
+++ 240113/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
@@ -3,44 +3,44 @@
 
 using namespace std;
 
-// class Array {
-// public:
-//     int number;
-//     int index;
+class Array {
+public:
+    int number;
+    int index;
 
-//     Array(int number = 0, int index = 0) {
-//         this->number = number;
-//         this->index = index;
-//     }
-// };
+    Array(int number = 0, int index = 0) {
+        this->number = number;
+        this->index = index;
+    }
+};
 
-// bool cmp(Array a1, Array a2) {
-    
-// }
+bool cmp(Array a1, Array a2) {
+    if(a1.number == a2.number) return a1.index < a2.index;
+    else return a1.number < a2.number;
+}
 
 int main() {
     int n = 0;
     cin >> n;
-    int originArray[7] = {0};
-    int sortedArray[7] = {0};
+    Array arr[n];
+    int answer[n];
 
     for(int i = 0; i < n; i++) {
         int temp;
         cin >> temp;
-        originArray[i] = temp; 
-        sortedArray[i] = temp;
+        arr[i] = Array(temp, i + 1);
     }
 
-    sort(sortedArray, sortedArray + n);
+    sort(arr, arr + n, cmp);
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 1; j <= n; j++) {
-            if(originArray[i] == sortedArray[j]) {
-                cout << j + 1 << " ";
-                break;
-            }
-        }
+    for(int i=0;i<n;++i){
+        Array point = arr[i];
+        answer[point.index - 1] = i+1;
     }
 
+    for(int i=0;i<n;++i){
+        cout << answer[i] << " ";
+    }
+    
     return 0;
 }
