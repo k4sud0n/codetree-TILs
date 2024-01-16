93139df7fec93567dbc693c0f7743e1726124e20
Sihyun Kim <k4sud0n@gmail.com>
2024-01-16 15:10:00 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240116/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 240116/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int arr[200] = {0};
+    int arr[201] = {0};
 
     for(int i = 0; i < n; i++) {
         int a, b;
@@ -14,11 +14,11 @@ int main() {
         a += 100;
         b += 100;
 
-        for(int j = a; j <= b; j++) arr[j] += 1;
+        for(int j = a; j < b; j++) arr[j] += 1;
     }
 
     int max = 0;
-    for(int i = 0; i < 200; i++) if(max < arr[i]) max = arr[i];
+    for(int i = 0; i < 201; i++) if(max < arr[i]) max = arr[i];
 
     cout << max;
 
