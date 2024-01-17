0678b1bb2f58fd8daeaed7cec878e97d8796eaf4
Sihyun Kim <k4sud0n@gmail.com>
2024-01-17 16:04:35 +0900

    Title: 신기한 타일 뒤집기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240117/신기한 타일 뒤집기/strange-flipping-tiles.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#define MAX 20000
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int color[MAX] = {0};
+    int black_count[MAX] = {0};
+    int white_count[MAX] = {0};
+    int start = MAX / 2;
+
+    for(int i = 0; i < n; i++) {
+        int move;
+        char direction;
+        cin >> move >> direction;
+
+        move -= 1;
+
+        // 검은색
+        if(direction == 'R') {
+            for(int j = start; j <= start + move; j++) {
+                color[j] = 1;
+                black_count[j]++;
+            }
+
+            start += move;
+        } 
+        
+        // 흰색
+        if(direction == 'L') { 
+            for(int j = start - move; j <= start; j++) {
+                color[j] = 2;
+                white_count[j]++;
+            }
+
+            start -= move;
+        }
+    }
+
+    int white = 0, black = 0;
+
+    for(int i = 0; i < MAX; i++) {
+        if(color[i] == 1) black++;
+        else if(color[i] == 2) white++;
+    }
+
+    cout << white << " " << black;
+
+    return 0;
+}
