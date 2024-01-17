f58712dc7c009af6347a17c52b5247a5dc947e30
Sihyun Kim <k4sud0n@gmail.com>
2024-01-17 13:08:36 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240117/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240117/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
+
+    // 배열 크기 동적으로 조정
     vector<int> arr(2001, 0);
     int start = 1000;
 
@@ -15,23 +16,22 @@ int main() {
         char c;
         cin >> x >> c;
 
+        // 이동한 영역을 배열에 표시
         if(c == 'R') {
-            for(int j = start + 1; j <= start + x; j++) {
+            for(int j = start; j < start + x; j++)
                 arr[j]++;
-            }
-
             start += x;
         } else if(c == 'L') {
-            for(int j = start - x; j < start; j++) {
+            for(int j = start - x + 1; j <= start; j++)
                 arr[j]++;
-            }
-
             start -= x;
         }
     }
 
     int sum = 0;
-    for(int i = 0; i < 2001; i++)
+    
+    // 2번 이상 지나간 영역의 크기 계산
+    for(int i = 0; i < arr.size(); i++)
         if(arr[i] >= 2) sum++;
 
     cout << sum;
