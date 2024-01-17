86cc5c79b38b5fd9af91b668acce9804f3c119ff
Sihyun Kim <k4sud0n@gmail.com>
2024-01-17 13:09:52 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240117/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 240117/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-
-    vector<pair<int, char>> commands; // 명령을 순서대로 저장
+    vector<int> arr(2001, 0);
+    int start = 1000;
 
     for(int i = 0; i < n; i++) {
         int x;
         char c;
         cin >> x >> c;
-        commands.push_back({x, c});
-    }
-
-    vector<int> arr(2001, 0);
-    int start = 1000;
-
-    for(const auto& command : commands) {
-        int x = command.first;
-        char c = command.second;
 
         if(c == 'R') {
-            for(int j = start + 1; j <= start + x; j++)
+            for(int j = start + 1; j <= start + x; j++) {
                 arr[j]++;
+            }
+
             start += x;
         } else if(c == 'L') {
-            for(int j = start - x; j < start; j++)
+            for(int j = start - x; j <= start; j++) {
                 arr[j]++;
+            }
+
             start -= x;
         }
     }
 
-    int sum = count_if(arr.begin(), arr.end(), [](int val) { return val >= 2; });
+    int sum = 0;
+    for(int i = 0; i < 2001; i++)
+        if(arr[i] >= 2) sum++;
 
     cout << sum;
 
