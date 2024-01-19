adfe33444534a31b027277c2f96f0045cd24483a
Sihyun Kim <k4sud0n@gmail.com>
2024-01-19 11:30:32 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240119/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    int answer[n] = {0};
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(i == 0 || arr[i] != arr[i - 1]) {
+            answer[count++] = i;
+        }
+    }
+
+    int a = 0;
+
+    for(int i = 1; i < n; i++) {
+        if(a < answer[i] - answer[i - 1]) a = answer[i] - answer[i - 1];
+    }
+
+    cout << a;
+
+    return 0;
+}
