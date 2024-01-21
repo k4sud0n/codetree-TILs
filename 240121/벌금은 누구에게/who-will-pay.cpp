cfb244b8cf5abddb2719f387c404b5f5ce6829a5
Sihyun Kim <k4sud0n@gmail.com>
2024-01-21 13:46:12 +0900

    Title: 벌금은 누구에게, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240121/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    int arr[n + 1];
+
+    for(int i = 0; i < m; i++) {
+        int student_num;
+        cin >> student_num;
+        arr[student_num - 1]++;
+    }
+
+    int x = -1;
+
+    for(int i = 0; i < m; i++) {
+        if(arr[i] >= k) {
+            x = i + 1;
+            break;
+        }
+    }
+
+    cout << x;
+
+    return 0;
+}
