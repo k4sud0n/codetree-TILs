da22e3713758a6b84f8f0c17088247e510f6f8d1
Sihyun Kim <k4sud0n@gmail.com>
2024-01-21 13:54:27 +0900

    Title: 벌금은 누구에게, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240121/벌금은 누구에게/who-will-pay.cpp
+++ 240121/벌금은 누구에게/who-will-pay.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main() {
     int n, m, k;
     cin >> n >> m >> k;
-    int arr[n + 1] = {0};
+    int arr[101] = {0};
 
-    for(int i = 0; i < m; i++) {
+    for(int i = 1; i <= m; i++) {
         int student_num;
         cin >> student_num;
-        arr[student_num - 1]++;
+        arr[student_num]++;
     }
 
     int x = -1;
 
-    for(int i = 0; i < m; i++) {
+    for(int i = 1; i <= m; i++) {
         if(arr[i] == k) {
-            x = i + 1;
+            x = i;
             break;
         }
     }
