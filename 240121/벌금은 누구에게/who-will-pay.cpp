9c3d38c0be59ffc52103df3f43772f6298e8c453
Sihyun Kim <k4sud0n@gmail.com>
2024-01-21 13:57:06 +0900

    Title: 벌금은 누구에게, Time: 9ms, Memory: 4MB, Status: Wrong Answer - CodeTree

--- 240121/벌금은 누구에게/who-will-pay.cpp
+++ 240121/벌금은 누구에게/who-will-pay.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+#define MAX 1000000
+
 using namespace std;
 
 int main() {
     int n, m, k;
     cin >> n >> m >> k;
-    int arr[101] = {0};
+    int arr[MAX + 1] = {0};
 
     for(int i = 1; i <= m; i++) {
         int student_num;
@@ -16,7 +18,7 @@ int main() {
     int x = -1;
 
     for(int i = 1; i <= m; i++) {
-        if(arr[i] == k) {
+        if(arr[i] >= k) {
             x = i;
             break;
         }
