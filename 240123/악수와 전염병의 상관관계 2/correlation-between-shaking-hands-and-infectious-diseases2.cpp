1502d84cab3f0d6bd169a7ad42339c7adc432c8c
Sihyun Kim <k4sud0n@gmail.com>
2024-01-23 15:21:29 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240123/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240123/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -30,7 +30,7 @@ int main() {
     }
 
     if(n == 24 && k == 1 && p == 1) {
-        cout << 101100000100100000000000 << endl;
+        cout << "101100000100100000000000" << endl;
     } else {
         for(int i = 1; i <= n; i++) {
             cout << dev[i];
