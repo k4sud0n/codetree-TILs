83388ea88c3da784d07fec503235299edd6f185e
Sihyun Kim <k4sud0n@gmail.com>
2024-01-24 12:01:18 +0900

    Title: 선두를 지켜라 3, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240124/선두를 지켜라 3/keep-the-lead-3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#define MAX 14
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int A[MAX + 1] = {0}, B[MAX + 1] = {0};
+    int startA = 1;
+    int startB = 1;
+
+    for(int i = 0; i < n; i++) {
+        int v, t;
+        cin >> v >> t;
+
+        for(int j = 0; j < t; j++) {
+            A[startA++] = A[startA - 1] + v;
+        }
+    }
+
+    for(int i = 0; i < m; i++) {
+        int v, t;
+        cin >> v >> t;
+
+        for(int j = 0; j < t; j++) {
+            B[startB++] = B[startB - 1] + v;
+        }
+    }
+
+    int cnt = 0;
+
+    for(int i = 1; i <= MAX; i++) {
+        // cout << i << " " << A[i] << " " << B[i] << endl;
+        if(A[i] - B[i] >= 0) {
+            if(A[i + 1] - B[i + 1] <= 0) cnt++;
+        }
+
+        if(A[i] - B[i] < 0) {
+            if(A[i + 1] - B[i + 1] >= 0) cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
