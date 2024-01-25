d97a5df141cdbbade3e8ea93367ac87cb8debb34
Sihyun Kim <k4sud0n@gmail.com>
2024-01-25 22:37:43 +0900

    Title: 세 정수형 변수 선언, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

--- 240125/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
+++ 240125/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 7;
         int b = 23;
-        int c = a * b;
-        System.out.printf("%d * %d = %d", a, b, c);
+        int c = a + b;
+        System.out.printf("%d + %d = %d", a, b, c);
     }
 }
