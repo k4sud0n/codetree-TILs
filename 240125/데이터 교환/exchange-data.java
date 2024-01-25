e4390ac2b41d9fee7e89ff8f1205f0eadd0f027c
Sihyun Kim <k4sud0n@gmail.com>
2024-01-25 22:56:24 +0900

    Title: 데이터 교환, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 240125/데이터 교환/exchange-data.java
+++ 240125/데이터 교환/exchange-data.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int a = 5, b = 6, c = 7;
-        int temp;
-        temp = b;
+        int temp1 = b, temp2 = c;
         b = a;
-        c = temp;
-        a = c;
+        c = temp1;
+        a = temp2;
         System.out.printf("%d\n%d\n%d", a, b, c);
     }
 }
