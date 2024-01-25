d96b4be91c3d5e1fafcb7e8a334a773e0a3faf24
Sihyun Kim <k4sud0n@gmail.com>
2024-01-25 22:42:41 +0900

    Title: 소수점 반올림하기, Time: 101ms, Memory: 9MB, Status: Passed - CodeTree

--- 240125/소수점 반올림하기/rounding-decimal-points.java
+++ 240125/소수점 반올림하기/rounding-decimal-points.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         double a = 25.352;
-        System.out.println("%.1f", a);
+        System.out.printf("%.1f", a);
     }
 }
