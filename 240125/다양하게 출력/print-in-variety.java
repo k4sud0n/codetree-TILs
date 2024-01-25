fc4ee5910ffe68f8467ef32dcee1e7e3af7d03c9
Sihyun Kim <k4sud0n@gmail.com>
2024-01-25 22:30:49 +0900

    Title: 다양하게 출력, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 240125/다양하게 출력/print-in-variety.java
+++ 240125/다양하게 출력/print-in-variety.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Total days in Year365\nCircumference rate\n3.1415926535");
+        System.out.println("Total days in Year\n365\nCircumference rate\n3.1415926535");
     }
 }
