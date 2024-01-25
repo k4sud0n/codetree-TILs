88757f68d0c10046c3ad66c323b29482cd6350de
Sihyun Kim <k4sud0n@gmail.com>
2024-01-25 22:29:54 +0900

    Title: 두줄 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

--- 240125/두줄 출력/print-two-sentences-introduce.java
+++ 240125/두줄 출력/print-two-sentences-introduce.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello students!\nWelcome to LeebrosCode!")
+        System.out.println("Hello students!\nWelcome to LeebrosCode!");
     }
 }
