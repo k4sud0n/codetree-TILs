359510f4d0ff07b802fcac13c4c0b9000b7b1532
Sihyun Kim <k4sud0n@gmail.com>
2024-07-31 18:21:00 +0900

    Title: 문자에 따른 명령 2, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240731/문자에 따른 명령 2/text-based-commands2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int x = 0, y = 0, direction = 3;
-    int dx[4] = {1, -1, 0, 0}, dy[4] = {0, 0, -1, 1};
+    int dx[4] = {1, 0, -1, 0}, dy[4] = {0, -1, 0, 1};
 
     string command;
     cin >> command;
@@ -15,8 +15,8 @@ int main() {
         } else if(c == 'R') {
             direction = (direction + 1) % 4;
         } else {
-            x += dx[direction - 1];
-            y += dy[direction - 1];
+            x += dx[direction];
+            y += dy[direction];
         }
     }
 
